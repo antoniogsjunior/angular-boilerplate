b2e49bf240b05bb8c7cc9d8a104e54e1e3165a39
Junior <junior4g4@gmail.com>
2021-11-15 12:03:45 -0300

    feat: add storybook

--- src/app/views/auth/auth.module.ts
+++ src/app/views/auth/auth.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { ButtonModule } from '../../shared/components/button/button.module';
 import { AuthRoutingModule } from './auth.routing.module';
 import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  imports: [CommonModule, AuthRoutingModule],
+  imports: [CommonModule, AuthRoutingModule, ButtonModule],
   declarations: [LoginComponent]
 })
 export class AuthModule {}
